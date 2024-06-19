c15410d40ea1b3bd0c83123e05729ad36bb4976d
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-19 14:50:23 +0300

    finished core and extension

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import LeftMenu from "./left-menu";
+import RightMenu from "./right-menu";
+function Header() {
+  return (
+    <header>
+      <LeftMenu />
+      <RightMenu />
+    </header>
+  );
+}
+
+export default Header;
